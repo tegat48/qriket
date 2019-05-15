36c7c3da20eb64046315557ee9655be3ef0edf6b
tegat48 <45688062+tegat48@users.noreply.github.com>
2019-05-15 16:13:06 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+$email = "tpray740@gmail.com";
+$password = "Polokop11212";
+
+//Pillih Warna 0 untuk hijau 1 untuk kuning
+$collor = "0";
+
+// Pilih Jumlah Spin Saran 1 - 5
+$amount = "2";
+
+
+
